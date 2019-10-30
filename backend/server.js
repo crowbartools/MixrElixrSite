5a4a701bb6914a4a89c63cb466156b3ba451efe7
Firebottle <mage@gamingobscura.com>
2019-10-29 21:43:40 -0500

    Helmet

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet')
 const cors = require('cors');
 
 require('dotenv').config();
@@ -7,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
+app.use(helmet()); // Use first.
 app.use(cors());
 app.use(express.json());
 
